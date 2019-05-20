1d25462315bf09671676db20780f380b144b5963
user <hunchan12@gmail.com>
2019-05-20 18:31:32 +0900

    React-natvie-instagram initial commit

---
+++ screens/Search.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from '../styles.js'
+
+export default class Search extends React.Component {
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text> Search </Text>
+
+            </View>
+        );
+    }
+}
+
