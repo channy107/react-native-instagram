ea2cf4de793fc940db35faf21c30b0649240361a
user <hunchan12@gmail.com>
2019-05-21 08:53:16 +0900

    Add redux code

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import TabNavigator from './navigation/TabNavigator.js'
+import reducer from './reducers/index.js'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(reducers);
+
 
 export default class App extends React.Component {
 
   render() {
     return (
-      <TabNavigator />
+      <Provider store={store}>
+        <TabNavigator />
+      </Provider>
     );
   }
 }
